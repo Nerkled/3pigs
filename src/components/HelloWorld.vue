4a0351e109ae3686913123d3a39f8bd9d8d4b2be
Henry <henrybecker1014@gmail.com>
2025-01-13 10:59:25 -0500

    Initializing 3pigs repo
    Basic vue start with some changes.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="pink">{{ msg }}</h1>
+    <h3>
+      You made it piglets!
+      <a href="https://3pigs.bandcamp.com/album/when-pigs-fly" target="_blank" rel="noopener">M u s i c</a> +
+      <a href="https://www.instagram.com/3pigs.wav" target="_blank" rel="noopener">I n s t a</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/StoreView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import StoreItem from '../components/StoreItem.vue'
+</script>
+
+<template>
+  <main>
+    <StoreItem msg="   
+      (_)      
+ _ __  _  __ _ 
+| '_ \| |/ _` |
+| |_) | | (_| |
+| .__/|_|\__, |
+| |       __/ |
+|_|      |___/ 
+    " />
+  </main>
+</template>
